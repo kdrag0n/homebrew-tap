0b61fde23a1ee07228ac72558efb47a3e10972db
Danny Lin <danny@kdrag0n.dev>
2026-05-05 11:20:35 -0700

    Add hush Homebrew formula

---
+++ Formula/hush.rb
@@ -0,0 +1,56 @@
+class Hush < Formula
+  desc "Modern fuss-free SSH over QUIC"
+  homepage "https://github.com/kdrag0n/hush"
+  version "0.1.0"
+  license "MIT"
+
+  if OS.mac? && Hardware::CPU.arm?
+    url "https://github.com/kdrag0n/hush/releases/download/prerelease-main/hush-hush-macos-aarch64",
+        using: :nounzip
+    sha256 "a9feb2e99166e3613ea60860c13fc56b8dcdd3cde8a32c0982042ff32a9c1b8f"
+
+    resource "hush-server" do
+      url "https://github.com/kdrag0n/hush/releases/download/prerelease-main/hush-server-hush-macos-aarch64",
+          using: :nounzip
+      sha256 "eeaaf847ef33b4d31a118a0293005c206a408d6cbec1e236fb1e0470290a4e5c"
+    end
+  elsif OS.linux? && Hardware::CPU.arm?
+    url "https://github.com/kdrag0n/hush/releases/download/prerelease-main/hush-hush-linux-aarch64-musl",
+        using: :nounzip
+    sha256 "d485fb2ee8bf9594ee76f09a07ba428c4a8cad8c79cbab6ea1abe7f8a463d383"
+
+    resource "hush-server" do
+      url "https://github.com/kdrag0n/hush/releases/download/prerelease-main/hush-server-hush-linux-aarch64-musl",
+          using: :nounzip
+      sha256 "7ab4ee3710daa191940334b54513c38d34b5a9f9cfb7f31dfafca7491622bdfe"
+    end
+  elsif OS.linux? && Hardware::CPU.intel?
+    url "https://github.com/kdrag0n/hush/releases/download/prerelease-main/hush-hush-linux-x86_64-musl",
+        using: :nounzip
+    sha256 "8f208b9c5a0151d82db8d622eb02e36db7183f81ba2acdc024baef93e6d1b27b"
+
+    resource "hush-server" do
+      url "https://github.com/kdrag0n/hush/releases/download/prerelease-main/hush-server-hush-linux-x86_64-musl",
+          using: :nounzip
+      sha256 "8a14879911fa98da06391990651498b59d61a5ec36a6523cd6798dd04316fbf2"
+    end
+  else
+    odie "hush prebuilt binaries are not available for this platform"
+  end
+
+  def install
+    bin.install cached_download => "hush"
+    bin.install resource("hush-server").cached_download => "hush-server"
+  end
+
+  service do
+    run [opt_bin/"hush-server"]
+    keep_alive true
+    require_root true
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/hush --version")
+    assert_match version.to_s, shell_output("#{bin}/hush-server --version")
+  end
+end
